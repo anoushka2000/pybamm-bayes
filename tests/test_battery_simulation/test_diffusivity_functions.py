434334bd4507112d6bf5af5d4406c27b1d524019
anoushkabhutani <anoushkabhutani@gmail.com>
2022-07-20 00:45:08 -0400

    add tests for battery_simulation module

---
+++ tests/test_battery_simulation/test_diffusivity_functions.py
@@ -0,0 +1,20 @@
+import unittest
+from battery_model_parameterization.Python.battery_simulation.diffusivity_functions import *
+from pybamm.expression_tree.binary_operators import Multiplication
+
+
+class TestDiffusivityFunctions(unittest.TestCase):
+    def test_electrolyte_diffusivity_Capiglia1999(self):
+        c_e, T = 1, 280
+        result = electrolyte_diffusivity_Capiglia1999(c_e, T)
+        self.assertIsInstance(result, Multiplication)
+
+    def test_graphite_mcmb2528_diffusivity_Dualfoil1998(self):
+        sto, T = 0.8, 280
+        result = graphite_mcmb2528_diffusivity_Dualfoil1998(sto, T)
+        self.assertIsInstance(result, Multiplication)
+
+    def test_lico2_diffusivity_Dualfoil1998(self):
+        sto, T = 0.8, 280
+        result = lico2_diffusivity_Dualfoil1998(sto, T)
+        self.assertIsInstance(result, Multiplication)
