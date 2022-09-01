da712f820390f579a028258c770fcbfd000ddac4
anoushkabhutani <anoushkabhutani@gmail.com>
2022-09-01 18:29:00 -0400

    add `gelman_rubin_convergence_test` to check for convergence

--- battery_model_parameterization/Python/analysis.py
+++ battery_model_parameterization/Python/analysis.py
@@ -51,7 +51,7 @@ def load_metadata(logs_dir_name=None, logs_dir_path=None):
     return metadata
 
 
-def load_chains(logs_dir_path, concat = True):
+def load_chains(logs_dir_path, concat=True):
     """
     Parameters
     ----------
@@ -659,3 +659,73 @@ def plot_confidence_intervals(logs_dir_name, chi_sq_limit=10):
         fig = _plot_confidence_intervals_grid(logs_dir_name, n_variables, chi_sq_limit)
 
     return fig
+
+
+def gelman_rubin_convergence_test(logs_dir_name=None, logs_dir_path=None, burnin=500):
+    if logs_dir_path is None:
+        logs_dir_path = _get_logs_path(logs_dir_name)
+    metadata = load_metadata(logs_dir_path=logs_dir_path)
+    variable_names = [
+        var['name'] for var in metadata["variables"]
+    ]
+
+    chains = load_chains(logs_dir_path=logs_dir_path, concat=False)
+    gelman_rubin_factors = []
+    n_chains = len(chains)
+
+    for parameter in chains[0].columns:
+
+        chain_mean_list = []
+        chain_var_list = []
+
+        for chain in chains:
+            chain = chain[burnin:]
+
+            p_chain = chain[parameter]
+
+            # posterior mean of parameter
+            p_mean = p_chain.mean()
+            chain_mean_list.append(p_mean)
+
+            # variance of samples in chain for parameter
+            intra_chain_var = ((p_chain - p_mean) ** 2).sum() / (len(p_chain) - 1)
+            chain_var_list.append(intra_chain_var)
+
+        n_valid_iterations = len(chain)
+        mean_all_chains = sum(chain_mean_list) / len(chain_mean_list)
+        B = (n_valid_iterations / (n_chains - 1)) * ((chain_mean_list - mean_all_chains) ** 2).sum()
+        W = sum(chain_var_list) / len(chain_var_list)
+        V = ((n_valid_iterations - 1) / n_valid_iterations) * W + (n_chains + 1) / (n_chains * n_valid_iterations) * B
+        gelman_rubin_factors.append(V)
+        chains = analysis.load_chains(get_logs_path(logs_dir_name), concat=False)
+
+    gelman_rubin_factors = []
+
+    n_chains = len(chains)
+
+    for parameter in chains[0].columns:
+
+        chain_mean_list = []
+        chain_var_list = []
+
+        for chain in chains:
+            chain = chain[burnin:]
+
+            p_chain = chain[parameter]
+
+            # posterior mean of parameter
+            p_mean = p_chain.mean()
+            chain_mean_list.append(p_mean)
+
+            # variance of samples in chain for parameter
+            intra_chain_var = ((p_chain - p_mean) ** 2).sum() / (len(p_chain) - 1)
+            chain_var_list.append(intra_chain_var)
+
+        n_valid_iterations = len(chain)
+        mean_all_chains = sum(chain_mean_list) / len(chain_mean_list)
+        B = (n_valid_iterations / (n_chains - 1)) * ((chain_mean_list - mean_all_chains) ** 2).sum()
+        W = sum(chain_var_list) / len(chain_var_list)
+        V = ((n_valid_iterations - 1) / n_valid_iterations) * W + (n_chains + 1) / (n_chains * n_valid_iterations) * B
+        gelman_rubin_factors.append(V)
+
+    return dict(zip(variable_names, gelman_rubin_factors))
