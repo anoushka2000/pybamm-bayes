bb908d591509cda4b59b7e7c5739be3256b92df1
anoushkabhutani <anoushkabhutani@gmail.com>
2022-09-09 13:53:05 -0400

    update `Variable` class attribute `true_value` to `value`

--- battery_model_parameterization/Python/variable.py
+++ battery_model_parameterization/Python/variable.py
@@ -6,14 +6,15 @@ class Variable:
     ----------
     name: str
         Variable name
-    true_value: float
-        Value of variable used to simulate data.
+    value: float
+        For NonlinearIdentifiability problems this is the value of the variable used to simulate data.
+        For ParameterEstimation problems this is the value around which sampling chains are initialized.
     prior : pints.LogPrior
         Prior from which variable is sampled.
     """
 
-    def __init__(self, name, true_value, prior):
+    def __init__(self, name, value, prior):
         self.name = name
-        self.true_value = true_value
+        self.value = value
         self.prior = prior
         self.prior_type = str(type(self.prior)).split(".")[-1][:-2]
