88ab85abc63076c8b1c94925a9b8a0271e7135e6
anoushkabhutani <anoushkabhutani@gmail.com>
2022-09-15 19:25:48 -0400

    update positive exchange current density functions to use partial

--- battery_model_parameterization/Python/workflows/utils/parameters/lithium_ion/negative_electrode/exchange_current_density.py
+++ battery_model_parameterization/Python/workflows/utils/parameters/lithium_ion/negative_electrode/exchange_current_density.py
@@ -1,11 +1,14 @@
 #
-# Modifies pybamm exchange current density functions to allow j0_n as an input.
+# Modifies pybamm exchange current density functions to allow j0_n
+# and/ or alpha_n as inputs.
 #
 import pybamm
 from functools import partial
 
 
-def _graphite_electrolyte_exchange_current_density_Dualfoil1998(c_e, c_s_surf, T, alpha_input, j0_input):
+def _graphite_electrolyte_exchange_current_density_Dualfoil1998(
+    c_e, c_s_surf, T, alpha_input, j0_input
+):
     """
     Modifies pybamm function to allow j0_n and alpha_n input.
     Exchange-current density for Butler-Volmer reactions between graphite and LiPF6 in
@@ -15,15 +18,21 @@ def _graphite_electrolyte_exchange_current_density_Dualfoil1998(c_e, c_s_surf, T
     .. [2] http://www.cchem.berkeley.edu/jsngrp/fortran.html
     Parameters
     ----------
-    c_e : :class:`pybamm.Symbol`
+    c_e: pybamm.Symbol
         Electrolyte concentration [mol.m-3]
-    c_s_surf : :class:`pybamm.Symbol`
+    c_s_surf:pybamm.Symbol
         Particle concentration [mol.m-3]
-    T : :class:`pybamm.Symbol`
+    T: pybamm.Symbol
         Temperature [K]
+    alpha_input: bool
+        If True set alpha (Butler-Volmer charge tranfer coefficient)
+        as an input.
+    j0_input: bool
+        If True set j0 (reference exchange current density)
+        as an input.
     Returns
     -------
-    :class:`pybamm.Symbol`
+    pybamm.Symbol
         Exchange-current density [A.m-2]
     """
     m_ref, alpha = 2 * 10 ** (-5), 0.5
@@ -37,21 +46,25 @@ def _graphite_electrolyte_exchange_current_density_Dualfoil1998(c_e, c_s_surf, T
     c_n_max = pybamm.Parameter("Maximum concentration in negative electrode [mol.m-3]")
 
     return (
-            m_ref
-            * arrhenius
-            * c_e ** alpha
-            * c_s_surf ** alpha
-            * (c_n_max - c_s_surf) ** alpha
+        m_ref
+        * arrhenius
+        * c_e**alpha
+        * c_s_surf**alpha
+        * (c_n_max - c_s_surf) ** alpha
     )
 
 
 def graphite_electrolyte_exchange_current_density_Dualfoil1998(alpha_input, j0_input):
-    return partial(_graphite_electrolyte_exchange_current_density_Dualfoil1998,
-                   alpha_input=alpha_input,
-                   j0_input=j0_input)
+    return partial(
+        _graphite_electrolyte_exchange_current_density_Dualfoil1998,
+        alpha_input=alpha_input,
+        j0_input=j0_input,
+    )
 
 
-def _graphite_LGM50_electrolyte_exchange_current_density_Chen2020(c_e, c_s_surf, T, alpha_input, j0_input):
+def _graphite_LGM50_electrolyte_exchange_current_density_Chen2020(
+    c_e, c_s_surf, T, alpha_input, j0_input
+):
     """
     Exchange-current density for Butler-Volmer reactions between graphite and LiPF6 in
     EC:DMC.
@@ -63,15 +76,21 @@ def _graphite_LGM50_electrolyte_exchange_current_density_Chen2020(c_e, c_s_surf,
     Electrochemical Society 167 (2020): 080534.
     Parameters
     ----------
-    c_e : :class:`pybamm.Symbol`
+    c_e: pybamm.Symbol
         Electrolyte concentration [mol.m-3]
-    c_s_surf : :class:`pybamm.Symbol`
+    c_s_surf: pybamm.Symbol
         Particle concentration [mol.m-3]
-    T : :class:`pybamm.Symbol`
+    T: pybamm.Symbol
         Temperature [K]
+    alpha_input: bool
+        If True set alpha (Butler-Volmer charge tranfer coefficient)
+        as an input.
+    j0_input: bool
+        If True set j0 (reference exchange current density)
+        as an input.
     Returns
     -------
-    :class:`pybamm.Symbol`
+    pybamm.Symbol
         Exchange-current density [A.m-2]
     """
     m_ref, alpha = 6.48e-7, 0.5
@@ -87,21 +106,25 @@ def _graphite_LGM50_electrolyte_exchange_current_density_Chen2020(c_e, c_s_surf,
     c_n_max = pybamm.Parameter("Maximum concentration in negative electrode [mol.m-3]")
 
     return (
-            m_ref
-            * arrhenius
-            * c_e ** alpha
-            * c_s_surf ** alpha
-            * (c_n_max - c_s_surf) ** alpha
+        m_ref
+        * arrhenius
+        * c_e**alpha
+        * c_s_surf**alpha
+        * (c_n_max - c_s_surf) ** alpha
     )
 
 
 def graphite_LGM50_electrolyte_exchange_current_density_Chen2020(alpha_input, j0_input):
-    return partial(graphite_LGM50_electrolyte_exchange_current_density_Chen2020,
-                   alpha_input=alpha_input,
-                   j0_input=j0_input)
+    return partial(
+        graphite_LGM50_electrolyte_exchange_current_density_Chen2020,
+        alpha_input=alpha_input,
+        j0_input=j0_input,
+    )
 
 
-def _graphite_electrolyte_exchange_current_density_PeymanMPM(c_e, c_s_surf, c_s_max, T, alpha_input, j0_input):
+def _graphite_electrolyte_exchange_current_density_PeymanMPM(
+    c_e, c_s_surf, c_s_max, T, alpha_input, j0_input
+):
     """
     Exchange-current density for Butler-Volmer reactions between graphite and LiPF6 in
     EC:DMC.
@@ -111,40 +134,48 @@ def _graphite_electrolyte_exchange_current_density_PeymanMPM(c_e, c_s_surf, c_s_
     .. [2] http://www.cchem.berkeley.edu/jsngrp/fortran.html
     Parameters
     ----------
-    c_e : :class:`pybamm.Symbol`
+    c_e: pybamm.Symbol
         Electrolyte concentration [mol.m-3]
-    c_s_surf : :class:`pybamm.Symbol`
+    c_s_surf: pybamm.Symbol
         Particle concentration [mol.m-3]
-    c_s_max : :class:`pybamm.Symbol`
+    c_s_max: pybamm.Symbol
         Maximum particle concentration [mol.m-3]
-    T : :class:`pybamm.Symbol`
+    T: pybamm.Symbol
         Temperature [K]
+    alpha_input: bool
+        If True set alpha (Butler-Volmer charge tranfer coefficient)
+        as an input.
+    j0_input: bool
+        If True set j0 (reference exchange current density)
+        as an input.
     Returns
     -------
-    :class:`pybamm.Symbol`
+    pybamm.Symbol
         Exchange-current density [A.m-2]
     """
     m_ref, alpha = 1.061 * 10 ** (-6), 0.5
+    # m_ref units are (A/m2)(mol/m3)**1.5 - includes ref concentrations
+
     if j0_input:
         m_ref = pybamm.InputParameter("j0_n")
     if alpha_input:
         alpha = pybamm.InputParameter("alpha_n")
-    # m_ref = 1.061 * 10 ** (-6)   (unit has been converted)
-    # units are (A/m2)(mol/m3)**1.5 - includes ref concentrations
 
     E_r = 37480
     arrhenius = pybamm.exp(E_r / pybamm.constants.R * (1 / 298.15 - 1 / T))
 
     return (
-            m_ref
-            * arrhenius
-            * c_e ** alpha
-            * c_s_surf ** alpha
-            * (c_s_max - c_s_surf) ** alpha
+        m_ref
+        * arrhenius
+        * c_e**alpha
+        * c_s_surf**alpha
+        * (c_s_max - c_s_surf) ** alpha
     )
 
 
 def graphite_electrolyte_exchange_current_density_PeymanMPM(alpha_input, j0_input):
-    return partial(_graphite_electrolyte_exchange_current_density_PeymanMPM,
-                   alpha_input=alpha_input,
-                   j0_input=j0_input)
+    return partial(
+        _graphite_electrolyte_exchange_current_density_PeymanMPM,
+        alpha_input=alpha_input,
+        j0_input=j0_input,
+    )
--- battery_model_parameterization/Python/workflows/utils/parameters/lithium_ion/positive_electrode/exchange_current_density.py
+++ battery_model_parameterization/Python/workflows/utils/parameters/lithium_ion/positive_electrode/exchange_current_density.py
@@ -1,11 +1,15 @@
 #
-# Modifies pybamm exchange current density functions to allow j0_p as an input.
+# Modifies pybamm exchange current density functions to allow j0_p
+# and/or alpha_p as inputs.
 #
 
 import pybamm
+from functools import partial
 
 
-def lico2_electrolyte_exchange_current_density_Dualfoil1998(c_e, c_s_surf, T):
+def _lico2_electrolyte_exchange_current_density_Dualfoil1998(
+    c_e, c_s_surf, T, alpha_input, j0_input
+):
     """
     Exchange-current density for Butler-Volmer reactions between lico2 and LiPF6 in
     EC:DMC.
@@ -14,21 +18,29 @@ def lico2_electrolyte_exchange_current_density_Dualfoil1998(c_e, c_s_surf, T):
     .. [2] http://www.cchem.berkeley.edu/jsngrp/fortran.html
     Parameters
     ----------
-    c_e : :class:`pybamm.Symbol`
+    c_e: pybamm.Symbol
         Electrolyte concentration [mol.m-3]
-    c_s_surf : :class:`pybamm.Symbol`
+    c_s_surf: pybamm.Symbol
         Particle concentration [mol.m-3]
-    T : :class:`pybamm.Symbol`
+    T: pybamm.Symbol
         Temperature [K]
+    alpha_input: bool
+        If True set alpha (Butler-Volmer charge tranfer coefficient)
+        as an input.
+    j0_input: bool
+        If True set j0 (reference exchange current density)
+        as an input.
     Returns
     -------
-    :class:`pybamm.Symbol`
+    pybamm.Symbol
         Exchange-current density [A.m-2]
     """
-    m_ref = pybamm.InputParameter(
-        "j0_p"
-    )  # default = 6 * 10 ** (-7) (A/m2)(mol/m3)**1.5 - includes ref concentrations
-    alpha = pybamm.InputParameter("alpha_p")  # default = 0.5
+    m_ref, alpha = 6 * 10 ** (-7), 0.5
+
+    if j0_input:
+        m_ref = pybamm.InputParameter("j0_p")
+    if alpha_input:
+        alpha = pybamm.InputParameter("alpha_p")  # default = 0.5
     E_r = 39570
     arrhenius = pybamm.exp(E_r / pybamm.constants.R * (1 / 298.15 - 1 / T))
 
@@ -43,81 +55,17 @@ def lico2_electrolyte_exchange_current_density_Dualfoil1998(c_e, c_s_surf, T):
     )
 
 
-def lico2_electrolyte_exchange_current_density_Dualfoil1998_j0(c_e, c_s_surf, T):
-    """
-    Exchange-current density for Butler-Volmer reactions between lico2 and LiPF6 in
-    EC:DMC.
-    References
-    ----------
-    .. [2] http://www.cchem.berkeley.edu/jsngrp/fortran.html
-    Parameters
-    ----------
-    c_e : :class:`pybamm.Symbol`
-        Electrolyte concentration [mol.m-3]
-    c_s_surf : :class:`pybamm.Symbol`
-        Particle concentration [mol.m-3]
-    T : :class:`pybamm.Symbol`
-        Temperature [K]
-    Returns
-    -------
-    :class:`pybamm.Symbol`
-        Exchange-current density [A.m-2]
-    """
-    m_ref = pybamm.InputParameter(
-        "j0_p"
-    )  # default = 6 * 10 ** (-7) (A/m2)(mol/m3)**1.5 - includes ref concentrations
-    alpha = 0.5
-    E_r = 39570
-    arrhenius = pybamm.exp(E_r / pybamm.constants.R * (1 / 298.15 - 1 / T))
-
-    c_p_max = pybamm.Parameter("Maximum concentration in positive electrode [mol.m-3]")
-
-    return (
-        m_ref
-        * arrhenius
-        * c_e**alpha
-        * c_s_surf**alpha
-        * (c_p_max - c_s_surf) ** alpha
+def lico2_electrolyte_exchange_current_density_Dualfoil1998(alpha_input, j0_input):
+    return partial(
+        _lico2_electrolyte_exchange_current_density_Dualfoil1998,
+        alpha_input=alpha_input,
+        j0_input=j0_input,
     )
 
 
-def lico2_electrolyte_exchange_current_density_Dualfoil1998_alpha(c_e, c_s_surf, T):
-    """
-    Exchange-current density for Butler-Volmer reactions between lico2 and LiPF6 in
-    EC:DMC.
-    References
-    ----------
-    .. [2] http://www.cchem.berkeley.edu/jsngrp/fortran.html
-    Parameters
-    ----------
-    c_e : :class:`pybamm.Symbol`
-        Electrolyte concentration [mol.m-3]
-    c_s_surf : :class:`pybamm.Symbol`
-        Particle concentration [mol.m-3]
-    T : :class:`pybamm.Symbol`
-        Temperature [K]
-    Returns
-    -------
-    :class:`pybamm.Symbol`
-        Exchange-current density [A.m-2]
-    """
-    m_ref = 6 * 10 ** (-7)  # (A/m2)(mol/m3)**1.5 - includes ref concentrations
-    alpha = pybamm.InputParameter("alpha_p")  # default = 0.5
-    E_r = 39570
-    arrhenius = pybamm.exp(E_r / pybamm.constants.R * (1 / 298.15 - 1 / T))
-
-    c_p_max = pybamm.Parameter("Maximum concentration in positive electrode [mol.m-3]")
-
-    return (
-        m_ref
-        * arrhenius
-        * c_e**alpha
-        * c_s_surf**alpha
-        * (c_p_max - c_s_surf) ** alpha
-    )
-
-
-def nmc_LGM50_electrolyte_exchange_current_density_Chen2020(c_e, c_s_surf, T):
+def _nmc_LGM50_electrolyte_exchange_current_density_Chen2020(
+    c_e, c_s_surf, T, alpha_input, j0_input
+):
     """
     Exchange-current density for Butler-Volmer reactions between NMC and LiPF6 in
     EC:DMC.
@@ -129,22 +77,30 @@ def nmc_LGM50_electrolyte_exchange_current_density_Chen2020(c_e, c_s_surf, T):
     Electrochemical Society 167 (2020): 080534.
     Parameters
     ----------
-    c_e : :class:`pybamm.Symbol`
+    c_e: pybamm.Symbol
         Electrolyte concentration [mol.m-3]
-    c_s_surf : :class:`pybamm.Symbol`
+    c_s_surf: pybamm.Symbol
         Particle concentration [mol.m-3]
-    T : :class:`pybamm.Symbol`
+    T: pybamm.Symbol
         Temperature [K]
+    alpha_input: bool
+        If True set alpha (Butler-Volmer charge tranfer coefficient)
+        as an input.
+    j0_input: bool
+        If True set j0 (reference exchange current density)
+        as an input.
     Returns
     -------
-    :class:`pybamm.Symbol`
+    pybamm.Symbol
         Exchange-current density [A.m-2]
     """
-    m_ref = pybamm.InputParameter("j0_p")
-    # default value: m_ref = 3.42e-6 (A/m2)(mol/m3)**1.5
+    m_ref, alpha = 3.42e-6, 0.5
     # includes ref concentrations
 
-    alpha = pybamm.InputParameter("alpha_p")
+    if j0_input:
+        m_ref = pybamm.InputParameter("j0_p")
+    if alpha_input:
+        alpha = pybamm.InputParameter("alpha_p")
 
     E_r = 17800
     arrhenius = pybamm.exp(E_r / pybamm.constants.R * (1 / 298.15 - 1 / T))
@@ -160,93 +116,17 @@ def nmc_LGM50_electrolyte_exchange_current_density_Chen2020(c_e, c_s_surf, T):
     )
 
 
-def nmc_LGM50_electrolyte_exchange_current_density_Chen2020_j0(c_e, c_s_surf, T):
-    """
-    Exchange-current density for Butler-Volmer reactions between NMC and LiPF6 in
-    EC:DMC.
-    References
-    ----------
-    .. [1] Chang-Hui Chen, Ferran Brosa Planella, Kieran O’Regan, Dominika Gastol, W.
-    Dhammika Widanage, and Emma Kendrick. "Development of Experimental Techniques for
-    Parameterization of Multi-scale Lithium-ion Battery Models." Journal of the
-    Electrochemical Society 167 (2020): 080534.
-    Parameters
-    ----------
-    c_e : :class:`pybamm.Symbol`
-        Electrolyte concentration [mol.m-3]
-    c_s_surf : :class:`pybamm.Symbol`
-        Particle concentration [mol.m-3]
-    T : :class:`pybamm.Symbol`
-        Temperature [K]
-    Returns
-    -------
-    :class:`pybamm.Symbol`
-        Exchange-current density [A.m-2]
-    """
-    m_ref = pybamm.InputParameter("j0_p")
-    # default value: m_ref = 3.42e-6 (A/m2)(mol/m3)**1.5
-    # includes ref concentrations
-
-    alpha = 0.5
-
-    E_r = 17800
-    arrhenius = pybamm.exp(E_r / pybamm.constants.R * (1 / 298.15 - 1 / T))
-
-    c_p_max = pybamm.Parameter("Maximum concentration in positive electrode [mol.m-3]")
-
-    return (
-        m_ref
-        * arrhenius
-        * c_e**alpha
-        * c_s_surf**alpha
-        * (c_p_max - c_s_surf) ** alpha
+def nmc_LGM50_electrolyte_exchange_current_density_Chen2020(alpha_input, j0_input):
+    return partial(
+        _nmc_LGM50_electrolyte_exchange_current_density_Chen2020,
+        alpha_input=alpha_input,
+        j0_input=j0_input,
     )
 
 
-def nmc_LGM50_electrolyte_exchange_current_density_Chen2020_alpha(c_e, c_s_surf, T):
-    """
-    Exchange-current density for Butler-Volmer reactions between NMC and LiPF6 in
-    EC:DMC.
-    References
-    ----------
-    .. [1] Chang-Hui Chen, Ferran Brosa Planella, Kieran O’Regan, Dominika Gastol, W.
-    Dhammika Widanage, and Emma Kendrick. "Development of Experimental Techniques for
-    Parameterization of Multi-scale Lithium-ion Battery Models." Journal of the
-    Electrochemical Society 167 (2020): 080534.
-    Parameters
-    ----------
-    c_e : :class:`pybamm.Symbol`
-        Electrolyte concentration [mol.m-3]
-    c_s_surf : :class:`pybamm.Symbol`
-        Particle concentration [mol.m-3]
-    T : :class:`pybamm.Symbol`
-        Temperature [K]
-    Returns
-    -------
-    :class:`pybamm.Symbol`
-        Exchange-current density [A.m-2]
-    """
-
-    m_ref = 3.42e-6  # (A/m2)(mol/m3)**1.5
-    # includes ref concentrations
-
-    alpha = pybamm.InputParameter("alpha_p")
-
-    E_r = 17800
-    arrhenius = pybamm.exp(E_r / pybamm.constants.R * (1 / 298.15 - 1 / T))
-
-    c_p_max = pybamm.Parameter("Maximum concentration in positive electrode [mol.m-3]")
-
-    return (
-        m_ref
-        * arrhenius
-        * c_e**alpha
-        * c_s_surf**alpha
-        * (c_p_max - c_s_surf) ** alpha
-    )
-
-
-def NMC_electrolyte_exchange_current_density_PeymanMPM(c_e, c_s_surf, c_s_max, T):
+def _NMC_electrolyte_exchange_current_density_PeymanMPM(
+    c_e, c_s_surf, c_s_max, T, alpha_input, j0_input
+):
     """
     Exchange-current density for Butler-Volmer reactions between NMC and LiPF6 in
     EC:DMC.
@@ -255,23 +135,33 @@ def NMC_electrolyte_exchange_current_density_PeymanMPM(c_e, c_s_surf, c_s_max, T
     .. Peyman MPM manuscript (to be submitted)
     Parameters
     ----------
-    c_e : :class:`pybamm.Symbol`
+    c_e: pybamm.Symbol
         Electrolyte concentration [mol.m-3]
-    c_s_surf : :class:`pybamm.Symbol`
+    c_s_surf: pybamm.Symbol
         Particle concentration [mol.m-3]
-    c_s_max : :class:`pybamm.Symbol`
+    c_s_max: pybamm.Symbol
         Maximum particle concentration [mol.m-3]
-    T : :class:`pybamm.Symbol`
+    T: pybamm.Symbol
         Temperature [K]
+    alpha_input: bool
+        If True set alpha (Butler-Volmer charge tranfer coefficient)
+        as an input.
+    j0_input: bool
+        If True set j0 (reference exchange current density)
+        as an input.
+
     Returns
     -------
-    :class:`pybamm.Symbol`
+    pybamm.Symbol
         Exchange-current density [A.m-2]
     """
-    m_ref = pybamm.InputParameter("j0_p")
-    # default: m_ref = 4.824 * 10 ** (-6)
-    # (A/m2)(mol/m3)**1.5 - includes ref concentrations
-    alpha = pybamm.InputParameter("alpha_p")
+    m_ref, alpha = 4.824 * 10 ** (-6), 0.5
+
+    # m_ref (A/m2)(mol/m3)**1.5 - includes ref concentrations
+    if j0_input:
+        m_ref = pybamm.InputParameter("j0_p")
+    if alpha_input:
+        alpha = pybamm.InputParameter("alpha_p")
     E_r = 39570
     arrhenius = pybamm.exp(E_r / pybamm.constants.R * (1 / 298.15 - 1 / T))
 
@@ -284,77 +174,9 @@ def NMC_electrolyte_exchange_current_density_PeymanMPM(c_e, c_s_surf, c_s_max, T
     )
 
 
-def NMC_electrolyte_exchange_current_density_PeymanMPM_j0(c_e, c_s_surf, c_s_max, T):
-    """
-    Exchange-current density for Butler-Volmer reactions between NMC and LiPF6 in
-    EC:DMC.
-    References
-    ----------
-    .. Peyman MPM manuscript (to be submitted)
-    Parameters
-    ----------
-    c_e : :class:`pybamm.Symbol`
-        Electrolyte concentration [mol.m-3]
-    c_s_surf : :class:`pybamm.Symbol`
-        Particle concentration [mol.m-3]
-    c_s_max : :class:`pybamm.Symbol`
-        Maximum particle concentration [mol.m-3]
-    T : :class:`pybamm.Symbol`
-        Temperature [K]
-    Returns
-    -------
-    :class:`pybamm.Symbol`
-        Exchange-current density [A.m-2]
-    """
-    m_ref = pybamm.InputParameter("j0_p")
-    # default: m_ref = 4.824 * 10 ** (-6)
-    # (A/m2)(mol/m3)**1.5 - includes ref concentrations
-    alpha = 0.5
-    E_r = 39570
-    arrhenius = pybamm.exp(E_r / pybamm.constants.R * (1 / 298.15 - 1 / T))
-
-    return (
-        m_ref
-        * arrhenius
-        * c_e**alpha
-        * c_s_surf**alpha
-        * (c_s_max - c_s_surf) ** alpha
-    )
-
-
-def NMC_electrolyte_exchange_current_density_PeymanMPM_alpha(c_e, c_s_surf, c_s_max, T):
-    """
-    Exchange-current density for Butler-Volmer reactions between NMC and LiPF6 in
-    EC:DMC.
-    References
-    ----------
-    .. Peyman MPM manuscript (to be submitted)
-    Parameters
-    ----------
-    c_e : :class:`pybamm.Symbol`
-        Electrolyte concentration [mol.m-3]
-    c_s_surf : :class:`pybamm.Symbol`
-        Particle concentration [mol.m-3]
-    c_s_max : :class:`pybamm.Symbol`
-        Maximum particle concentration [mol.m-3]
-    T : :class:`pybamm.Symbol`
-        Temperature [K]
-    Returns
-    -------
-    :class:`pybamm.Symbol`
-        Exchange-current density [A.m-2]
-    """
-    # m_ref = pybamm.InputParameter("j0_p")
-    m_ref = 4.824 * 10 ** (-6)
-    # (A/m2)(mol/m3)**1.5 - includes ref concentrations
-    alpha = pybamm.InputParameter("alpha_p")
-    E_r = 39570
-    arrhenius = pybamm.exp(E_r / pybamm.constants.R * (1 / 298.15 - 1 / T))
-
-    return (
-        m_ref
-        * arrhenius
-        * c_e**alpha
-        * c_s_surf**alpha
-        * (c_s_max - c_s_surf) ** alpha
+def NMC_electrolyte_exchange_current_density_PeymanMPM(alpha_input, j0_input):
+    return partial(
+        _NMC_electrolyte_exchange_current_density_PeymanMPM,
+        alpha_input=alpha_input,
+        j0_input=j0_input,
     )
