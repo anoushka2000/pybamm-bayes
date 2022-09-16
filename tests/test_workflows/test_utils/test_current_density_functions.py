b92c86d0471937207950a8dd536e8bbcf756f6ee
anoushkabhutani <anoushkabhutani@gmail.com>
2022-09-15 20:09:02 -0400

    correct test inputs

--- tests/test_workflows/test_utils/test_current_density_functions.py
+++ tests/test_workflows/test_utils/test_current_density_functions.py
@@ -14,46 +14,49 @@ from pybamm.expression_tree.binary_operators import Multiplication
 class TestCurrentDensityFunctions(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.test_values = 1, 1, 280  # c_e, c_s_surf, T
+        cls.c_e = 1
+        cls.c_s_surf = 1
+        cls.c_s_max = 1
+        cls.T = 280
 
     def test_lico2_electrolyte_exchange_current_density_Dualfoil1998(self):
         f = lico2_electrolyte_exchange_current_density_Dualfoil1998(
             alpha_input=False, j0_input=False
         )
-        result = f(*self.test_values)
+        result = f(self.c_e, self.c_s_surf, self.T)
         self.assertIsInstance(result, Multiplication)
 
     def test_nmc_LGM50_electrolyte_exchange_current_density_Chen2020(self):
         f = nmc_LGM50_electrolyte_exchange_current_density_Chen2020(
             alpha_input=False, j0_input=False
         )
-        result = f(*self.test_values)
+        result = f(self.c_e, self.c_s_surf, self.T)
         self.assertIsInstance(result, Multiplication)
 
     def NMC_electrolyte_exchange_current_density_PeymanMPM(self):
         f = NMC_electrolyte_exchange_current_density_PeymanMPM(
             alpha_input=False, j0_input=False
         )
-        result = f(*self.test_values)
+        result = f(self.c_e, self.c_s_surf, self.T)
         self.assertIsInstance(result, Multiplication)
 
     def test_graphite_electrolyte_exchange_current_density_Dualfoil1998(self):
         f = graphite_electrolyte_exchange_current_density_Dualfoil1998(
             alpha_input=False, j0_input=False
         )
-        result = f(*self.test_values)
+        result = f(self.c_e, self.c_s_surf, self.T)
         self.assertIsInstance(result, Multiplication)
 
     def test_graphite_LGM50_electrolyte_exchange_current_density_Chen2020(self):
         f = graphite_LGM50_electrolyte_exchange_current_density_Chen2020(
             alpha_input=False, j0_input=False
         )
-        result = f(*self.test_values)
+        result = f(self.c_e, self.c_s_surf, self.T)
         self.assertIsInstance(result, Multiplication)
 
     def test_graphite_electrolyte_exchange_current_density_PeymanMPM(self):
         f = graphite_electrolyte_exchange_current_density_PeymanMPM(
             alpha_input=False, j0_input=False
         )
-        result = f(*self.test_values)
+        result = f(self.c_e, self.c_s_surf, self.c_s_max, self.T)
         self.assertIsInstance(result, Multiplication)
