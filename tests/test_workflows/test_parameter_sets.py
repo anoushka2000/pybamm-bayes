24b4bef54c55ead3fda43712967f7803b6789115
anoushkabhutani <anoushkabhutani@gmail.com>
2022-09-25 00:20:47 -0400

    fix tests

--- tests/test_workflows/test_parameter_sets.py
+++ tests/test_workflows/test_parameter_sets.py
@@ -1,5 +1,6 @@
 import unittest
 
+import functools
 import pints
 import pybamm
 from battery_model_parameterization import (
@@ -32,18 +33,30 @@ class TestModelSetup(unittest.TestCase):
             param[
                 "Positive electrode diffusivity [m2.s-1]"
             ], pybamm.InputParameter)
+        self.assertIsInstance(
+            param[
+                "Positive electrode exchange-current density [A.m-2]"
+            ], functools.partial
+        )
+        self.assertIsInstance(
+            param[
+                "Negative electrode exchange-current density [A.m-2]"
+            ], functools.partial
+        )
 
     def test_marquis_2019(self):
         param = marquis_2019(self.variables)
         self.assertIsInstance(param, pybamm.ParameterValues)
         self.assertIsInstance(
             param[
-                "Negative electrode diffusivity [m2.s-1]"
-            ], pybamm.InputParameter)
+                "Positive electrode exchange-current density [A.m-2]"
+            ], functools.partial
+        )
         self.assertIsInstance(
             param[
-                "Positive electrode diffusivity [m2.s-1]"
-            ], pybamm.InputParameter)
+                "Negative electrode exchange-current density [A.m-2]"
+            ], functools.partial
+        )
 
     def test_mohtat_2020(self):
         param = mohtat_2020(self.variables)
@@ -54,5 +67,11 @@ class TestModelSetup(unittest.TestCase):
             ], pybamm.InputParameter)
         self.assertIsInstance(
             param[
-                "Positive electrode diffusivity [m2.s-1]"
-            ], pybamm.InputParameter)
+                "Positive electrode exchange-current density [A.m-2]"
+            ], functools.partial
+        )
+        self.assertIsInstance(
+            param[
+                "Negative electrode exchange-current density [A.m-2]"
+            ], functools.partial
+        )
