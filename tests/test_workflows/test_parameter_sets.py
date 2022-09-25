e4d3fbd7176d43216d9875768f4d88e054982727
anoushkabhutani <anoushkabhutani@gmail.com>
2022-09-24 23:02:00 -0400

    update tests for parameter sets

--- tests/test_workflows/test_parameter_sets.py
+++ tests/test_workflows/test_parameter_sets.py
@@ -14,21 +14,27 @@ class TestModelSetup(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         # setup
-        log_prior_Dsn = pints.GaussianLogPrior(-13, 1)
-        log_prior_j0_n = pints.GaussianLogPrior(-4.26, 1)
-        Dsn = Variable(name="Ds_n", value=-13.45, prior=log_prior_Dsn)
-        j0_n = Variable(name="j0_n", value=-4.698, prior=log_prior_j0_n)
+        log_prior_D = pints.GaussianLogPrior(-13, 1)
 
-        cls.variables = [Dsn, j0_n]
+        Dsn = Variable(name="Ds_n", value=-13.45, prior=log_prior_D)
+        Dsp = Variable(name="Ds_p", value=-13.45, prior=log_prior_D)
+
+        cls.variables = [Dsn, Dsp]
 
     def test_chen_2020(self):
         param = chen_2020(self.variables)
         self.assertIsInstance(param, pybamm.ParameterValues)
+        self.assertIsInstance(param["Negative electrode diffusivity [m2.s-1]"], pybamm.InputParameter)
+        self.assertIsInstance(param["Positive electrode diffusivity [m2.s-1]"], pybamm.InputParameter)
 
     def test_marquis_2019(self):
         param = marquis_2019(self.variables)
         self.assertIsInstance(param, pybamm.ParameterValues)
+        self.assertIsInstance(param["Negative electrode diffusivity [m2.s-1]"], pybamm.InputParameter)
+        self.assertIsInstance(param["Positive electrode diffusivity [m2.s-1]"], pybamm.InputParameter)
 
     def test_mohtat_2020(self):
         param = mohtat_2020(self.variables)
         self.assertIsInstance(param, pybamm.ParameterValues)
+        self.assertIsInstance(param["Negative electrode diffusivity [m2.s-1]"], pybamm.InputParameter)
+        self.assertIsInstance(param["Positive electrode diffusivity [m2.s-1]"], pybamm.InputParameter)
