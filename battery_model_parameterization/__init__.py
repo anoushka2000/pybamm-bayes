d78c63af77e48b9581009e0c51c14f2e43146aff
anoushkabhutani <anoushkabhutani@gmail.com>
2022-12-21 16:08:54 -0500

    update top level imports in `__init__`

--- battery_model_parameterization/__init__.py
+++ battery_model_parameterization/__init__.py
@@ -1,17 +1,28 @@
 #
+# Base
+#
+from .Python.variable import Variable
+from .Python.analysis.utils import *
+
+#
 # Sampling Problems
 #
-# Analysis
-from .Python.analysis.load_results import *
-from .Python.analysis.plotting import *
-from .Python.analysis.utils import *
 from .Python.sampling_problems.base_sampling_problem import *
 from .Python.sampling_problems.bolfi_identifiability_analysis import *
 from .Python.sampling_problems.mcmc_identifiability_analysis import *
 from .Python.sampling_problems.mcmc_parameter_estimation import *
-from .Python.variable import Variable
+
+#
+# Analysis
+#
+from .Python.analysis.postprocessing import *
+from .Python.analysis.plotting import *
+from .Python.analysis.diagnostics import *
+
+#
+# Parameter Sets
+#
+from .Python.workflows.utils.parameter_sets.utils import *
 from .Python.workflows.utils.parameter_sets.chen2020 import *
 from .Python.workflows.utils.parameter_sets.marquis2019 import *
 from .Python.workflows.utils.parameter_sets.mohtat2020 import *
-# Parameter Sets
-from .Python.workflows.utils.parameter_sets.utils import *
