e79d701fd1126b90f5f95edf0cef40b5c5adcd89
anoushkabhutani <anoushkabhutani@gmail.com>
2022-12-21 16:29:20 -0500

    boilerplate for analysis module tests

---
+++ tests/test_analysis/test_utils.py
@@ -0,0 +1,19 @@
+import unittest
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+class TestUtils(unittest.TestCase):
+    sampling_problem = None
+
+    @classmethod
+    def setUpClass(cls):
+        # setup variables
+        pass
+
+    def test_sample_from_prior(self):
+        pass
+
+    def test_sample_from_posterior(self):
+        pass
