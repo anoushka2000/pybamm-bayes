2d7f711c5527a459783a15123f19e6f5e2025c6d
anoushkabhutani <anoushkabhutani@gmail.com>
2022-12-30 12:31:54 -0500

    fix tests

--- tests/test_sampling_problems/test_bolfi_identifiability_analysis.py
+++ tests/test_sampling_problems/test_bolfi_identifiability_analysis.py
@@ -20,9 +20,9 @@ class TestBOLFIIdentifiabilityAnalysis(unittest.TestCase):
         # setup variables
         prior_Ds_n = elfi.Prior("norm", 13, 1, name="Ds_n")
         prior_Ds_p = elfi.Prior("norm", 12.5, 1, name="Ds_p")
-        Ds_n = Variable(name="Ds_n", value=13.4, prior_loc=-13, prior_scale=1,
+        Ds_n = Variable(name="Ds_n", value=13.4, prior_loc=13, prior_scale=1,
                         prior=prior_Ds_n, bounds=(12, 14))
-        Ds_p = Variable(name="Ds_p", value=13, prior_loc=-13, prior_scale=1,
+        Ds_p = Variable(name="Ds_p", value=13, prior_loc=13, prior_scale=1,
                         prior=prior_Ds_p, bounds=(12, 14))
 
         cls.variables = [Ds_n, Ds_p]
