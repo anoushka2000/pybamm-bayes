ca8645988467210720a5c4e93feb051a5c21da9e
Anoushka Bhutani <abhutani@umich.edu>
2024-10-09 13:39:23 -0400

    todo: fix g-r stat

--- pybamm_bayes/analysis/diagnostics.py
+++ pybamm_bayes/analysis/diagnostics.py
@@ -3,95 +3,66 @@ import numpy as np
 from pybamm_bayes.analysis.postprocessing import load_chains, load_metadata
 from pybamm_bayes.analysis.utils import _get_logs_path
 
+# TODO: correct this
+# def gelman_rubin_convergence_test(logs_dir_name=None, logs_dir_path=None, burnin=500):
+#     """
+#     Gelman Rubin statistic to diagnose if a chain has converged.
+#     (If Gelman Rubin statistic < 1.1, parameter is identifiable).
 
-def gelman_rubin_convergence_test(logs_dir_name=None, logs_dir_path=None, burnin=500):
-    """
-    Gelman Rubin statistic to diagnose if a chain has converged.
-    (If Gelman Rubin statistic < 1.1, parameter is identifiable).
+#      Parameters
+#     ----------
+#     logs_dir_name: str
+#        Name of directory logging idenfiability problem results.
+#     logs_dir_path: str
+#         Absolute path to directory logging idenfiability problem results.
+#     burnin: int
+#         Number of samples to ignore as burn-in in calculation.
 
-     Parameters
-    ----------
-    logs_dir_name: str
-       Name of directory logging idenfiability problem results.
-    logs_dir_path: str
-        Absolute path to directory logging idenfiability problem results.
-    burnin: int
-        Number of samples to ignore as burn-in in calculation.
+#     Returns
+#     --------
+#     Dictionary with variable name as key and Gelman Rubin statistic as value.
+#     """
+#     if logs_dir_path is None:
+#         logs_dir_path = _get_logs_path(logs_dir_name)
+#     metadata = load_metadata(logs_dir_path=logs_dir_path)
+#     variable_names = [var["name"] for var in metadata["variables"]]
 
-    Returns
-    --------
-    Dictionary with variable name as key and Gelman Rubin statistic as value.
-    """
-    if logs_dir_path is None:
-        logs_dir_path = _get_logs_path(logs_dir_name)
-    metadata = load_metadata(logs_dir_path=logs_dir_path)
-    variable_names = [var["name"] for var in metadata["variables"]]
+#     chains = load_chains(logs_dir_path=logs_dir_path, concat=False)
+#     gelman_rubin_factors = []
+#     # Number of chains
+#     J = len(chains)
 
-    chains = load_chains(logs_dir_path=logs_dir_path, concat=False)
-    gelman_rubin_factors = []
-    n_chains = len(chains)
+#     for parameter in chains[0].columns:
 
-    for parameter in chains[0].columns:
+#         chain_mean_list = []
+#         chain_var_list = []
 
-        chain_mean_list = []
-        chain_var_list = []
+#         for chain in chains:
+#             chain = chain[burnin:]
 
-        for chain in chains:
-            chain = chain[burnin:]
+#             p_chain = chain[parameter].values
 
-            p_chain = chain[parameter]
+#             # mean of chain
+#             p_mean = p_chain.mean()
+#             chain_mean_list.append(p_mean)
 
-            # posterior mean of parameter
-            p_mean = p_chain.mean()
-            chain_mean_list.append(p_mean)
+#             # variance of samples in chain for parameter
+#             intra_chain_var = ((p_chain - p_mean) ** 2).sum()
+#             chain_var_list.append(intra_chain_var)
 
-            # variance of samples in chain for parameter
-            intra_chain_var = ((p_chain - p_mean) ** 2).sum() / (len(p_chain) - 1)
-            chain_var_list.append(intra_chain_var)
+#         L = len(chain)
+#         # mean of the means of all chains
+#         mean_all_chains = sum(chain_mean_list) / len(chain_mean_list)
+#         # variance of the means of the chains
+#         B = (L / (J - 1)) * (
+#             (np.array(chain_mean_list) - mean_all_chains) ** 2
+#         ).sum()
 
-        n_valid_iterations = len(chain)
-        mean_all_chains = sum(chain_mean_list) / len(chain_mean_list)
-        B = (n_valid_iterations / (n_chains - 1)) * (
-            (np.array(chain_mean_list) - mean_all_chains) ** 2
-        ).sum()
-        W = sum(chain_var_list) / len(chain_var_list)
-        V = ((n_valid_iterations - 1) / n_valid_iterations) * W + (n_chains + 1) / (
-            n_chains * n_valid_iterations
-        ) * B
-        gelman_rubin_factors.append(V)
-        chains = load_chains(logs_dir_path=logs_dir_path, concat=False)
+#         # averaged variances of the individual chains across all chains
+#         W = sum(chain_var_list) / len(chain_var_list)
+#         num = W*(L-1)/L + B/L
+#         R = num/W
+#         gelman_rubin_factors.append(R)
+#         chains = load_chains(logs_dir_path=logs_dir_path, concat=False)
 
-    gelman_rubin_factors = []
-
-    n_chains = len(chains)
-
-    for parameter in chains[0].columns:
-
-        chain_mean_list = []
-        chain_var_list = []
-
-        for chain in chains:
-            chain = chain[burnin:]
-
-            p_chain = chain[parameter]
-
-            # posterior mean of parameter
-            p_mean = p_chain.mean()
-            chain_mean_list.append(p_mean)
-
-            # variance of samples in chain for parameter
-            intra_chain_var = ((p_chain - p_mean) ** 2).sum() / (len(p_chain) - 1)
-            chain_var_list.append(intra_chain_var)
-
-        n_valid_iterations = len(chain)
-        mean_all_chains = sum(chain_mean_list) / len(chain_mean_list)
-        B = (n_valid_iterations / (n_chains - 1)) * (
-            (chain_mean_list - mean_all_chains) ** 2
-        ).sum()
-        W = sum(chain_var_list) / len(chain_var_list)
-        V = ((n_valid_iterations - 1) / n_valid_iterations) * W + (n_chains + 1) / (
-            n_chains * n_valid_iterations
-        ) * B
-        gelman_rubin_factors.append(V)
-
-    return dict(zip(variable_names, gelman_rubin_factors))
+#     return dict(zip(variable_names, gelman_rubin_factors))
